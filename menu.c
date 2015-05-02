7e1b06a53bea3322edc67031446d8b24cd05ff65
Thomas E. Dickey <dickey@invisible-island.net>
2015-05-02 11:52:27 -0400

    snapshot of project "tack", label t20150502

--- menu.c
+++ menu.c
@@ -1,5 +1,5 @@
 /*
-** Copyright (C) 1991, 1997 Free Software Foundation, Inc.
+** Copyright (C) 1991, 1997,2010 Free Software Foundation, Inc.
 ** 
 ** This file is part of TACK.
 ** 
@@ -21,7 +21,7 @@
 
 #include <tack.h>
 
-MODULE_ID("$Id: menu.c,v 1.8 2010/09/03 22:21:57 tom Exp $")
+MODULE_ID("$Id: menu.c,v 1.9 2010/09/03 22:21:57 tom Exp $")
 
 /*
    Menu control
